1e02f6c7799e600d7835f76ca96cb1ec382d57ce
Malafey Dmitry <malafeydmitry@Malafeys-Mac-mini.local>
2019-05-23 19:04:40 +0300

    feat: searchbox

--- src/SearchField.js
+++ src/SearchField.js
@@ -1,13 +1,30 @@
 import React from 'react';
-
+import ReactDOM from 'react-dom';
+import List from './List'
 
 class SearchField extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      list: ['first video', 'second video', 'last video'],
+      list: ["video 1", "second video", "last video"]
     };
 
+    this.removeItem = this.removeItem.bind(this);
+  }
+
+
+  removeItem(item) {
+    const list = this.state.list.slice();
+    list.some((el, i) => {
+      if (el === item) {
+      list.splice(i, 1);
+        return true;
+      }
+    });
+    // Set state to list
+    this.setState({
+      list: list
+    });
   }
 
   render() {
@@ -15,20 +32,7 @@ class SearchField extends React.Component {
       <div className="content">
         <div className="container">
           <section className="section">
-          </section>
-          <hr />
-          <section className="section">
-            <form className="form" id="addItemForm">
-              <input
-                type="text"
-                className="input"
-                id="addInput"
-                placeholder="Something that needs ot be done..."
-              />
-              <button className="button is-info" type="submit" onClick={this.addItem}>
-                ADD
-              </button>
-            </form>
+                        <List items={this.state.list} delete={this.removeItem} />
           </section>
         </div>
       </div>
